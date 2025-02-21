23b4575e3d2bc47d7d87c3e64c164cae71b1e5ee
Evan <e_phoukong@u.pacific.edu>
2025-02-21 14:19:05 -0800

    A timer object needs to be created with the number of ms that needs to be passed for it to wake and the object that should be woken passed to it. The class responding to the timer needs to implement ActionListener and an actionPerformed function. Finally, the timer needs to start using .start().

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,15 @@
 import acm.graphics.*;
 import acm.program.*;
+import java.awt.event.*;
+import javax.swing.Timer;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private int numTimes;
+	private Timer t;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -15,6 +19,21 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		
+		
+		t = new Timer(1000, this);
+		t.start();
+		
+		numTimes = 0;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		myLabel.setLabel("times called? " + numTimes);
+		myLabel.move(5, 0);
+		if (numTimes == 10) {
+			t.stop();
+		}
 	}
 	
 	public static void main(String[] args) {
